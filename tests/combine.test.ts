17ea663b8f5f45aa590d4650d98d6d213aa3d63d
Mark Evans <markevans3000@gmail.com>
2021-01-24 13:26:50 +0000

    tests to double-check combine's functionality

--- tests/combine.test.ts
+++ tests/combine.test.ts
@@ -1,10 +1,9 @@
-import { Observable } from "rxjs";
-import { atom } from "../src/atom";
+import { Atom, atom } from "../src/atom";
 import { combine } from "../src/combine";
 import { get } from "../src/get";
 
 describe("combine", () => {
-  let names$: Observable<string[]>, index$: Observable<number>;
+  let names$: Atom<string[]>, index$: Atom<number>;
 
   beforeEach(() => {
     names$ = atom(["Fred", "Joy", "Nelson"]);
@@ -29,5 +28,35 @@ describe("combine", () => {
   it("allows using an array and mapping", () => {
     const name$ = combine([names$, index$], ([names, index]) => names[index]);
     expect(get(name$)).toEqual("Joy");
+  });
+
+  it("only calculates once for multiple observers", () => {
+    const mapper = jest.fn(([names, index]) => names[index]);
+    const name$ = combine([names$, index$], mapper);
+
+    let a, b;
+    const sub1 = name$.subscribe((name) => (a = name));
+    const sub2 = name$.subscribe((name) => (b = name));
+
+    expect(a).toEqual("Joy");
+    expect(b).toEqual("Joy");
+    expect(mapper).toHaveBeenCalledTimes(1);
+
+    mapper.mockClear();
+    index$.set(0);
+    expect(a).toEqual("Fred");
+    expect(b).toEqual("Fred");
+    expect(mapper).toHaveBeenCalledTimes(1);
+
+    sub1.unsubscribe();
+    sub2.unsubscribe();
+  });
+
+  it("doesn't bother calculating if no-one is observing", () => {
+    const mapper = jest.fn(([names, index]) => names[index]);
+    combine([names$, index$], mapper);
+
+    index$.set(0);
+    expect(mapper).toHaveBeenCalledTimes(0);
   });
 });
