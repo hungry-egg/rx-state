742b938f7dee518e2c101bdf12b87029754db166
Mark Evans <markevans3000@gmail.com>
2021-03-17 11:14:27 +0000

    better jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   roots: ["src", "tests"],
-  transform: { "\\.ts$": "ts-jest" },
-  testRegex: "\\.test\\.ts$",
-  moduleFileExtensions: ["ts", "js", "json"],
-  modulePaths: ["<rootDir>/src"],
+  preset: "ts-jest",
 };
